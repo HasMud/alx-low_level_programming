53944212617525aaba117b87d9010b8e5574629b
Hassen Mudesir <hasram95@gmail.com>
2022-09-10 06:16:39 -0700

    c-hello_world Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 
 /**
- * main - Entry point
- *
+ * main - Prints out a string stdout 
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
