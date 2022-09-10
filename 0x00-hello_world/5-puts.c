d3b56f17595967ea95eb73eac67572a5ec1fb578
Hassen Mudesir <hasram95@gmail.com>
2022-09-10 01:39:33 -0700

    c-hello_world task

--- 0x00-hello_world/5-puts.c
+++ 0x00-hello_world/5-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /*
- * main - Entry point
+ * main - Prints - with proper grammar, but the outcome is a piece of art,
  *
  * Return: Always 0 (Success)
- *
- */int main(void)
+ */
+int main(void)
 {
-	printf(\n"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
