252198a09a8092e93bb524d90d0001f4be2dd900
Hassen Mudesir <hasram95@gmail.com>
2022-09-11 08:08:26 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 int d;
-for (d = 0; d < 89; d++)
+for (d = 0; d < 90; d++)
 {
 putchar((d / 10) + '0');
 putchar((d % 10) + '0');
 }
-if (d < 88)
+if (d < 89)
 {
 putchar(',');
 putchar(' ');
