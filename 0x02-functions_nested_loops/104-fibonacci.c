d7b013dfbb53b079a58e22b93e4fb89ca589219d
Hassen Mudesir <hasram95@gmail.com>
2022-09-14 07:44:08 -0700

    0x02-functions_nested_loops Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the first 98 Fibonacci numbers
+ * Function to prints
+ * Function to print
+ * Return: return 0
+ */
+void printFibonacciNumbers(int n)
+{
+int f1 = 1, f2 = 3, i;
+if (n < 1)
+return;
+printf("%d ", f1);
+for (i = 1; i < n; i++) {
+printf("%d ", f2);
+_putchar(',');
+int next = f1 + f2;
+f1 = f2;
+f2 = next;
+}
+}
+int main()
+{
+printFibonacciNumbers(98);
+return 0;
+}
