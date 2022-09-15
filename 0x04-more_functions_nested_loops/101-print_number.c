4ff5ee8c2e5cfa2b9ae39da1774b6d80dbe2760d
Hassen Mudesir <hasram95@gmail.com>
2022-09-15 13:47:14 -0700

    0x04-more_functions_nested_loops Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,7 +4,7 @@
  * print_number - Prints a number
  * @n: The number to print
  */
-void printnumber(int n)
+void print_number(int n)
 {
 if (n < 0) 
 {
@@ -14,6 +14,6 @@ n = -n;
 if (n == 0)
 putchar('0');
 if (n/10)
-printnumber(n/10);
+print_number(n/10);
 putchar(n%10 + '0');
 }
