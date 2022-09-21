7be99d220b79ed50785d1663096c1f0385411d7f
Hassen Mudesir <hasram95@gmail.com>
2022-09-21 10:34:42 -0700

    0x06-pointers_arrays_strings Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,11 +14,13 @@ char *_strcat(char *dest, char *src)
 	int index = 0, dest_len = 0;
 
 	while(dest[index++])
+	{
 		dest_len++;
-
+	}
 	for (index = 0; src[index]; index++)
+	{
 		dest[dest_len++] = srs[index];
-
+	}
 	return (dest);
 }
 
