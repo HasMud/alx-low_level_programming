2d7d6ca1ec2d1aa90caa0bc6256f87202d796d20
Hassen Mudesir <hasram95@gmail.com>
2022-09-30 13:19:59 -0700

    0x0A-argc_argv Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,11 +9,12 @@
  */
 int check_num(char *str)
 {
+/*Declaring variables*/
 unsigned int count;
 count = 0;
-while (count < strlen(str)) 
+while (count < strlen(str)) /*count string*/
 {
-if (!isdigit(str[count])) 
+if (!isdigit(str[count])) /*check if str there are digit*/
 {
 return (0);
 }
@@ -29,17 +30,19 @@ return (1);
  */
 int main(int argc, char *argv[])
 {
+/*Declaring variables*/ 
 int count;
 int str_to_int;
 int sum = 0;
 count = 1;
-while (count < argc) 
+while (count < argc) /*Goes through the whole array*/
 {
 if (check_num(argv[count]))
 {
-str_to_int = atoi(argv[count]); 
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
 }
+/*Condition if one of the number contains symbols that are not digits*/
 else
 {
 printf("Error\n");
@@ -47,6 +50,6 @@ return (1);
 }
 count++;
 }
-printf("%d\n", sum); 
+printf("%d\n", sum); /*print sum*/
 return (0);
 }
